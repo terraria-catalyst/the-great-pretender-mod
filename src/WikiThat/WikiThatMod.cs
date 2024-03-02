fbf0be455ce53b9870728de853c966ebac96bfc8
tomat <xxlennygamerxx@gmail.com>
2024-03-02 05:43:03 -0800

    Begin setting up "WikiThat".

---
+++ src/WikiThat/WikiThatMod.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using Terraria.ModLoader;
+
+namespace WikiThat {
+    [UsedImplicitly]
+    internal static class Unused { }
+}
+
+namespace TeamCatalyst.WikiThat {
+    public sealed class WikiThatMod : Mod { }
+}
